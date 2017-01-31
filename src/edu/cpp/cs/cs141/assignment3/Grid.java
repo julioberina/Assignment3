a95a747c7ab59c94feb58275d16f069533922d7c
Julio Berina <julioberina@gmail.com>
2017-01-31 08:45:31 -0800

    Implemented Grid class constructor, initializeCards, and display method

--- src/edu/cpp/cs/cs141/assignment3/Grid.java
+++ src/edu/cpp/cs/cs141/assignment3/Grid.java
@@ -5,10 +5,69 @@
  */
 package edu.cpp.cs.cs141.assignment3;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *
  * @author Julio
  */
 public class Grid {
+    private String[][] gridLayout;
+    private List<Card> cards;
     
+    public Grid()
+    {
+        gridLayout = new String[4][4];
+        
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+                gridLayout[x][y] = Integer.toString(x * 4 + (y + 1));
+        }
+        
+        initializeCards();
+    }
+    
+    public void initializeCards()
+    {
+        cards = new ArrayList<Card>();
+        
+        for (int i = 0; i < 16; i++)
+        {
+            if (i == 0 || i == 1)
+                cards.add(i, new QuestionCard());
+            else if (i == 2 || i == 3)
+                cards.add(i, new PlusCard());
+            else if (i == 4 || i == 5)
+                cards.add(i, new MinusCard());
+            else if (i == 6 || i == 7)
+                cards.add(i, new StarCard());
+            else if (i == 8 || i == 9)
+                cards.add(i, new BangCard());
+            else if (i == 10 || i == 11)
+                cards.add(i, new SlashCard());
+            else if (i == 12 || i == 13)
+                cards.add(i, new PoundCard());
+            else if (i == 14 || i == 15)
+                cards.add(i, new PercentCard());
+        }
+        
+        for (int x = 0; x < 5; x++) { Collections.shuffle(cards); }
+    }
+    
+    public void display()
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                if (y == 3)
+                    System.out.println(gridLayout[x][y]);
+                else
+                    System.out.print(gridLayout[x][y] + "\t");
+            }
+        }
+    }
 }
